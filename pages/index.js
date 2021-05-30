d8ac355e8720340ca4f6f5d88fe55a9e02650e40
Rob Cheatham <rob@decipher.studio>
2021-05-30 20:16:38 +0200

    Basic typography styles

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Dashboard() {
     return(
         <div>
             <h1>Performance Dashboard</h1>
+            <p>Check out what's going on in Production today.</p>
         </div>
     );
 }
