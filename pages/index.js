959c4ad31061bd8b968c16b47c14fabac1ad14a8
Rob Cheatham <rob@decipher.studio>
2021-05-31 13:10:29 +0200

    Links added to main paths

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+import Link from 'next/link';
+
 function Dashboard() {
+
+
+
     return(
         <div>
             <h1>Performance Dashboard</h1>
             <p>Check out what's going on in Production today.</p>
+            <ul>
+                <li>
+                    <Link href='/tasks'>All Tasks</Link>
+                </li>
+                <li>
+                    <Link href='/tasks/my-tasks'>My Tasks</Link>
+                </li>
+            </ul>
         </div>
     );
+
 }
 
 export default Dashboard;
